9b6bad9f32cf5dc01ca66da8c8299309c2d5eb56
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 20:54:54 +0300

    Update 8-merging.js

--- solutions/8-merging.js
+++ solutions/8-merging.js
@@ -1,5 +1,11 @@
 import _ from 'lodash';
 
 // BEGIN
-
+export default (objFirst, keys, objSecond) => {
+    if (keys.length === 0)
+        Object.assign(objFirst, objSecond);
+    else
+        Object.assign(objFirst, _.pick(objSecond, keys));
+    return objFirst;
+}
 // END
