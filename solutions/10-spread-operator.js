ca09d7d502a6def66275cb92f432db5cbb589000
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 20:56:04 +0300

    Update 10-spread-operator.js

--- solutions/10-spread-operator.js
+++ solutions/10-spread-operator.js
@@ -1,3 +1,9 @@
 // BEGIN
-
+export default (name, additionalProperties = {}) => {
+    const defaultProperties = {
+        state: 'moderating',
+        createdAt: Date.now()
+    };
+    return {name, ...defaultProperties, ...additionalProperties};
+}
 // END
