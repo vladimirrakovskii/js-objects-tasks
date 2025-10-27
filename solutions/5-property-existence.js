4d0b0c99ec34636acaf1be233f8b4738662c73ea
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 20:52:42 +0300

    Update 5-property-existence.js

--- solutions/5-property-existence.js
+++ solutions/5-property-existence.js
@@ -1,5 +1,18 @@
 import _ from 'lodash';
 
 // BEGIN
-
+export default (data) => {
+    if (data === "")
+        return {};
+    const obj = {};
+    const parts = data.split(' ');
+    for (let item of parts) {
+        item = item.toLowerCase();
+        if (Object.hasOwn(obj, item))
+            obj[item] += 1;
+        else
+            obj[item] = 1;
+    }
+    return obj;
+}
 // END
