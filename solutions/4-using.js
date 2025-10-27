24ac4bd5f7226f2413889ff597a6c7fcfb268c55
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 20:51:58 +0300

    Update 4-using.js

--- solutions/4-using.js
+++ solutions/4-using.js
@@ -1,3 +1,8 @@
 // BEGIN
-
+export default (name) => {
+    if (name.startsWith('https://')) {
+        return { scheme: 'https', name: name.replace('https://', '') };
+    }
+    return { scheme: 'http', name: name.replace('http://', '') };
+};
 // END
