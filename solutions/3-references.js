fb42a96c22c63387295345cf498548bf1c550171
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 20:44:01 +0300

    Update 3-references.js

--- solutions/3-references.js
+++ solutions/3-references.js
@@ -1,3 +1,9 @@
 // BEGIN
-
+export default (companyFirst, companySecond) => {
+    if ((companyFirst.name === companySecond.name) 
+        && (companyFirst.state === companySecond.state) 
+        && (companyFirst.website === companySecond.website))
+        return true;
+    return false;
+}
 // END
