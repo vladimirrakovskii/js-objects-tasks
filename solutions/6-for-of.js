cf5c9b89d12f4b25c0b26913fbbabc3c7473d0a3
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 20:54:04 +0300

    Update 6-for-of.js

--- solutions/6-for-of.js
+++ solutions/6-for-of.js
@@ -1,3 +1,9 @@
 // BEGIN
-
+export default (obj, keys) => {
+    const res = {};
+    for (const item of keys)
+        if (Object.hasOwn(obj, item))
+            res[item] = obj[item];
+    return res;
+}
 // END
