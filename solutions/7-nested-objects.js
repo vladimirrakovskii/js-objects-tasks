4910c45f7370cb6513aff0269e19c457b338cd1c
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 20:54:30 +0300

    Update 7-nested-objects.js

--- solutions/7-nested-objects.js
+++ solutions/7-nested-objects.js
@@ -1,3 +1,11 @@
 // BEGIN
-
+export default (obj, keys) => {
+    let newObj = obj;
+    for (const item of keys) {
+        if (!(Object.hasOwn(newObj, item)))
+            return null;
+        newObj = newObj[item];
+    }
+    return newObj;
+}
 // END
